1fa0427b03a23c6c7dbe570d9913cf184fd7a466
Kirsten Lindblom <kirsten.m.lindblom@gmail.com>
2020-01-09 14:23:49 -0800

    Implemented navbar on root app

--- src/App.js
+++ src/App.js
@@ -6,11 +6,30 @@ import CreateTodo from './components/create-todo.component';
 import TodosList from './components/todos-list.component';
 import EditTodo from './components/edit-todo.component';
 
+import logo from './logo.png';
+
 function App() {
     return (
         <Router>
             <div className="container">
-                <h2>MERN-stack To Do App</h2>
+                
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="https://github.com/kirstenmay" target="_blank">
+                        <img src={logo} width="30" height="30" alt="Kirstens gtihub"/>
+                    </a>
+                    <Link to="/" className="navbar-brand">MERN-Stack To Do App</Link>
+                    <div className="nav-collapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="navbar-item">
+                                <Link to="/" className="nav-link">To Do</Link>
+                            </li>
+                            <li className="navbar-item">
+                                <Link to="/create" className="nav-link">+To Do</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
                 <Route path="/" exact component={TodosList}/>
                 <Route path="/edit/:id" component={EditTodo}/>
                 <Route path="/create" component={CreateTodo}/>
